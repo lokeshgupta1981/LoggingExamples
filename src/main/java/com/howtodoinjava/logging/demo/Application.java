9ce30bceecb51f4978dd4a9cec8b9c9c949327ee
Lokesh Gupta <howtodoinjava@gmail.com>
2021-02-24 18:31:00 +0530

    Updated console appender example

--- src/main/java/com/howtodoinjava/logging/demo/Application.java
+++ src/main/java/com/howtodoinjava/logging/demo/Application.java
@@ -1,7 +1,7 @@
 package com.howtodoinjava.logging.demo;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -10,13 +10,13 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
-  private static final Logger LOGGER = LogManager.getLogger(Application.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
   public static void main(String[] args) {
     ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
     LOGGER.info("Info level log message");
     LOGGER.debug("Debug level log message");
-    LOGGER.error("Error level log message");
+    LOGGER.error("Error Message Logged !!!", new NullPointerException("NullError"));
   }
 }
